92f8a20194f5868705c94fae8dbad8330fe6cecd
Hernandes <lertanisslobr@gmail.com>
2021-12-15 19:17:14 -0300

    creating views of API

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,21 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.utils import serializer_helpers
 
+from quiz.models import Category, Question, Quiz
+from quiz.serializers import CategorySerializer, QuestionSerializer, QuizSerializer
 # Create your views here.
+
+class ListCreateAPICategory(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+class ListCreateAPIQuestion(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class= QuestionSerializer
+class CreateAPIQuiz(generics.CreateAPIView):
+    serializer_class = QuizSerializer
+    queryset = Quiz.objects.all()
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
