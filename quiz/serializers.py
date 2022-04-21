38a35a146a96a4ade3df538ebe82e79c10e386d0
Hernandes-Silva <lertanisslobr@gmail.com>
2022-04-21 12:15:05 -0300

    fix(quiz/serializers): colocando o class meta

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -2,11 +2,14 @@ from rest_framework import serializers
 from .models import Category, Question, Quiz
 
 class CategorySerializer(serializers.ModelSerializer):
-    model = Category
-    fields = '__all__'
+    class Meta:
+        model = Category
+        fields = '__all__'
 class QuestionSerializer(serializers.ModelSerializer):
-    model = Question
-    fields = '__all__'
+    class Meta:
+        model = Question
+        fields = '__all__'
 class QuizSerializer(serializers.ModelSerializer):
-    model = Quiz
-    exclude = ('user',)
+    class Meta:
+        model = Quiz
+        exclude = ('user',)
