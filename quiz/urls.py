280c62699166dcf140e04520b4fbf0e12e4a503b
Hernandes-Silva <lertanisslobr@gmail.com>
2022-04-22 11:37:42 -0300

    feat: adcionando ModelViewSet em category e question

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from quiz.views import ListCreateAPICategory, ListCreateAPIQuestion
+from rest_framework import routers
+from django.urls.conf import include
+
+router = routers.DefaultRouter()
+router.register(r'category', ListCreateAPICategory)
+router.register(r'question', ListCreateAPIQuestion)
+
 
 urlpatterns = [
-    path('category', ListCreateAPICategory.as_view(), name='category'),
-    path('question', ListCreateAPIQuestion.as_view(), name='question'),
+    path('', include(router.urls)),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,21 +1,25 @@
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.utils import serializer_helpers
 
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import random
 from quiz.models import Category, Question, Quiz
 from quiz.serializers import CategorySerializer, QuestionSerializer, QuizSerializer
 # Create your views here.
 
-class ListCreateAPICategory(generics.ListCreateAPIView):
+class ListCreateAPICategory(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-class ListCreateAPIQuestion(generics.ListCreateAPIView):
+
+class ListCreateAPIQuestion(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class= QuestionSerializer
+
 class CreateAPIQuiz(generics.CreateAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
     def perform_create(self,serializer):
         serializer.save(user=self.request.user)
-
+    
