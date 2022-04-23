328d08613ff44fd80ce08c7924caf8eee6ad5a81
Hernandes-Silva <lertanisslobr@gmail.com>
2022-04-23 19:25:10 -0300

    feat: criando a pasta de tests

--- quiz/views.py
+++ quiz/views.py
@@ -13,18 +13,30 @@ class ListCreateAPICategory(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    
 class ListCreateAPIQuestion(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class= QuestionSerializer
 
-@api_view(['GET', ])
+@api_view(['GET',])
 def generate_quiz(request, pk):
-
     questions = list(Question.objects.filter(categories = pk))
     if len(questions) >=10:
         questions = random.sample(questions, 10)
     serializer = QuestionSerializer(questions, many=True)
     
     return Response(serializer.data)
+
+""" @api_view(['POST',])
+def finish_quiz(request):
+    questions = request.data.getlist('questions')
+    category = request.data.get('category')
+    score = 0
+    if len(questions) <=10:
+        for question in questions:
+            correct = Question.objects.get(question['question_id']).answer == question['answer']
+            if correct """
+
+
+
+
     
