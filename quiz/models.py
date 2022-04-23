b79e53167c54829b5796acb1ca680f1f565cb759
Hernandes-Silva <lertanisslobr@gmail.com>
2022-04-23 19:17:40 -0300

    feat:adcionando o model QuizQuestion

--- quiz/models.py
+++ quiz/models.py
@@ -4,6 +4,8 @@ from django.db.models.fields import CharField
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+choice_answer = (('a','A'), ('b', 'B'), ('c', 'C'))
+
 class Category(models.Model):
     name = models.CharField(max_length=250)
 
@@ -12,7 +14,6 @@ class Category(models.Model):
 
 class Question(models.Model):
     categories = models.ManyToManyField(Category, related_name='questions')
-    choice_answer = (('a','A'), ('b', 'B'), ('c', 'C'))
     title = models.CharField(max_length=250)
     text = models.TextField()
     a = models.TextField()
@@ -20,8 +21,12 @@ class Question(models.Model):
     c = models.TextField()
     answer = models.CharField(max_length=1, choices=choice_answer)
 
+class QuizQuestion(models.Model):
+    question = models.ForeignKey(Question, related_name='quiz_question')
+    user_answer = models.CharField(max_length=1, choices=choice_answer)
+
 class Quiz(models.Model):
     user = models.ForeignKey(User, related_name='quizzes', on_delete=models.CASCADE)
-    questions = models.ManyToManyField(Question, related_name='quizzes')
+    questions = models.ManyToManyField(QuizQuestion, related_name='quiz')
     score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
 
