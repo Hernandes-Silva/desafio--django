dd55095cea931cd7da05355c912a0fad4a235ffa
Hernandes-Silva <lertanisslobr@gmail.com>
2022-04-24 12:05:37 -0300

    test(tests/conftest): trazendo fixture category_f

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
+from quiz.models import Category
 from rest_framework.test import APIClient
 
 @pytest.fixture
 def client():
     return APIClient()
+
+@pytest.fixture
+def category_f(db):
+    return Category.objects.create(name="Category")
