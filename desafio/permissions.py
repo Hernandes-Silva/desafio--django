105d8abea419a5616554e6b19e0cee11f56da289
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-03 11:09:09 -0300

    feat(desafio/permissions): modificando a permission IsAdminOrReadOnly

--- desafio/permissions.py
+++ desafio/permissions.py
@@ -1,17 +1,23 @@
 from rest_framework import permissions
+from django.contrib.auth.models import Group
 
 class IsAdminOrReadOnlyViewSet(permissions.BasePermission):
 
     def has_permission(self, request, view):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
+        print(request.method, view.action)
+        
         if request.method in permissions.SAFE_METHODS:
             if view.action == 'create':
                 return False
 
             return True
 
-        return request.user.is_superuser
+        group = Group.objects.get(name="admin_quiz")
+        print(group in request.user.groups.all())
+
+        return group in request.user.groups.all()   
 
 class IsAdminOrReadOnly(permissions.BasePermission):
 
@@ -22,4 +28,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return request.user.is_superuser
+        group = Group.objects.get(name="admin_quiz")
+        
+        return group in request.user.groups.all()
