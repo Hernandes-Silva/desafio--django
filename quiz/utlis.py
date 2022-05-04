f23ae4edd3bb54b9336052c2cba6b79da3d17dee
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-04 12:02:14 -0300

    feat(quiz/utils): criando a função  get_ranking_category

--- quiz/utlis.py
+++ quiz/utlis.py
@@ -8,6 +8,13 @@ def get_ranking_global():
     
     return ranking_global
 
+def get_ranking_category(category):
+    soma_score = Sum('score')
+    ranking_category = Quiz.objects.filter(category = category).values('user')\
+                        .annotate(score_t=soma_score).order_by('-score_t')
+    
+    return ranking_category
+
 def get_position_ranking_global(user):
     soma_score = Sum('score')
     ranking_global = Quiz.objects.all().values('user')\
