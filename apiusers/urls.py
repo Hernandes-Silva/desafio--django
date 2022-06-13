e4fd88cdfadb8967ffe102180b6dd6a1cb7547d5
Hernandes-Silva <lertanisslobr@gmail.com>
2022-06-12 23:22:51 -0300

    feat: create view for get user

--- apiusers/urls.py
+++ apiusers/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from django.urls.conf import include
 
-from apiusers.views import APICreateAdmin, APICreateUser
+from apiusers.views import APICreateAdmin, APICreateUser, current_user
 
 
 
 urlpatterns = [
     path('user-create/', APICreateUser.as_view(), name="user-create"),
-    path('admin-create/', APICreateAdmin.as_view(), name="admin-create")
+    path('admin-create/', APICreateAdmin.as_view(), name="admin-create"),
+    path("user/", current_user, name="current-user")
 ]
--- apiusers/views.py
+++ apiusers/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from apiusers.serializers import AdminSerializer, UserSerializer
 from desafio.permissions import IsAdmin, IsAdminOrReadOnly
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
 # Create your views here.
-
-
 class APICreateUser(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -15,4 +15,8 @@ class APICreateAdmin(CreateAPIView):
     permission_classes = [IsAuthenticated, IsAdmin]
     queryset = User.objects.all()
     serializer_class = AdminSerializer
-    
+
+@api_view(['GET'])
+def current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
