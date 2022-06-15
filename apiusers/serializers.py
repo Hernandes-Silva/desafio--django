008d4e4ea9bdee1688fe0d0403731e1fd9b0290d
Hernandes-Silva <lertanisslobr@gmail.com>
2022-06-15 12:44:25 -0300

    feat(apiusers/serializers): adding field groups for userSerializer

--- apiusers/serializers.py
+++ apiusers/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
 
+class GroupSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Group
+        fields = ('name',)
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
+    groups = GroupSerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ('username','first_name', 'last_name', 'email', 'password')
+        fields = ('username','first_name', 'last_name', 'email', 'password', 'groups')
 
     def create(self, validated_data):
         user = super(UserSerializer, self).create(validated_data)
